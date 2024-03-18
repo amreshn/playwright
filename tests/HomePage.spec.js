ffe5ee9ca96bc6e62d22757ac2f132687eff4829
amresh navalavan <amreshn45@gmail.com>
2024-03-18 18:40:36 +0000

    Test Scenario 3

--- tests/HomePage.spec.js
+++ tests/HomePage.spec.js
@@ -12,13 +12,13 @@ test("Test Scenario 1", async ({browser})=>{
     await expect(page.url()).toContain("simple-form-demo");
     await page.locator("[placeholder='Please enter your Message']").fill(textMessage);
     await page.locator("#showInput").click();
-    await expect(page.locator("#message")).toHaveText(textMessage);
+    const actualMessage = await page.locator("#message").textContent();
+    expect(actualMessage).toBe(textMessage);
 });
 
-test.only("Test Scenario 2", async ({page})=>{
+test("Test Scenario 2", async ({page})=>{
     const requiredValue = 95;
     const defaultValue = 15;
-    const textMessage = "Welcome to LambdaTest";
     await page.goto("https://www.lambdatest.com/selenium-playground/");
     await expect(page).toHaveTitle("Selenium Grid Online | Run Selenium Test On Cloud");
     await page.locator("[href*='drag-drop-range-sliders-demo']").click();
@@ -31,4 +31,49 @@ test.only("Test Scenario 2", async ({page})=>{
             break;
         }
     }
+});
+
+test("Test Scenario 3", async ({page})=>{
+    await page.goto("https://www.lambdatest.com/selenium-playground/");
+    await expect(page).toHaveTitle("Selenium Grid Online | Run Selenium Test On Cloud");
+    await page.locator("[href*='input-form-demo']").click();
+    expect(page.url()).toContain("input-form-demo");
+    const nameField = page.locator("#name");
+    const email = page.locator("#inputEmail4");
+    const password = page.locator("#inputPassword4");
+    const company = page.locator("#company");
+    const website = page.locator("#websitename");
+    const city = page.locator("#inputCity");
+    const address1 = page.locator("#inputAddress1");
+    const address2 = page.locator("#inputAddress2");
+    const state = page.locator("#inputState");
+    const zipCOde = page.locator("#inputZip");
+    const country = page.locator("[name='country']");
+    const submitButton = page.locator("text='Submit'");
+
+
+    await submitButton.click();
+    
+    const validationMessage = await nameField.evaluate((element) => {
+        const input = element;
+        return input.validationMessage;
+    });
+    expect(validationMessage).toBe("Please fill out this field.");
+
+    await nameField.fill("Sachin");
+    await email.fill("abc@gmail.com");
+    await password.fill("sachinSaga@#");
+    await company.fill("Arjun and Co.");
+    await website.fill("www.srtarj.com");
+    await country.selectOption("India");
+    await city.fill("Mumbai");
+    await address1.fill("Wankhade");
+    await address2.fill("Navi Mumbai");
+    await state.fill("Maharashtra");
+    await zipCOde.fill("6000145");
+    await submitButton.click();
+
+    await page.waitForSelector("text='Input form validations'");
+    const successMessage = await page.locator("[class*='success-msg']").textContent();
+    expect(successMessage).toBe("Thanks for contacting us, we will get back to you shortly.");
 });
